108b99e6e46719254c31fd3cc50c7e0be8a2d07f
Aleksandr Ovcharenko <shurik.asa@gmail.com>
2021-07-20 17:58:30 +0700

    Updating the script to be run correctly. The starting balance can be taken out to the config file and accounted for in the script

--- run_script.sh
+++ run_script.sh
@@ -9,7 +9,7 @@ while read -r line; do
         interval_filename="interval_list.txt"
         while read -r line; do
             interval="$line"
-            eval "python backtest.py ${interval} ${coin} ${bridge} 2>&1| tee run_logs/${coin}${bridge}_${interval}m_0426_0714.log"
+            eval "python backtest.py ${interval} ${coin} ${bridge} 100 2>&1| tee run_logs/${coin}${bridge}_${interval}m_0426_0714.log"
         done < "$interval_filename"
     done < "$coin_filename"
 done < "$bridge_filename"
